80a07785d4425deba1f7a1cc13e6cf2be063a2f6
M.S. <silkinmch@gmail.com>
2025-06-05 11:58:44 +0100

    finalisation of six

--- app/controllers/user_lessons_controller.rb
+++ app/controllers/user_lessons_controller.rb
@@ -1,4 +1,5 @@
 # app/controllers/user_lessons_controller.rb
+# [...]
 class UserLessonsController < ApplicationController
   # Add id to the list of actions below
   # before_action :set_user_lesson, only: %i[id]
@@ -6,4 +7,10 @@ class UserLessonsController < ApplicationController
   def show
     @user_lesson = UserLesson.find(params[:id])
   end
+
+  private
+
+  # def user_lesson_params
+    # params.require(:user_lesson).permit(:name, :number, :concept, :description, :level)
+  # end
 end
